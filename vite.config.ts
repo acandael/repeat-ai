ce3e6ea7190de4b2fadfad106532e199e3788abe
Anthony Candaele <info@anthonycandaele.com>
2025-02-16 08:54:18 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import Icons from 'unplugin-icons/vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		Icons({
+			compiler: 'svelte',
+			autoInstall: true
+		})
+	],
+	server: {
+		fs: {
+			// Allow access to files from the project root.
+			allow: ['..']
+		}
+	}
+});
